114ddf81c49f77d018dd8c69437156a28311e783
Chaedie <88099590+Chaedie@users.noreply.github.com>
2022-06-10 22:15:02 +0900

    Feat Creat, Read

--- server.js
+++ server.js
@@ -1,23 +1,44 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
-// listen
-app.listen(8080, function () {
-  console.log('listening on 8080');
+const mongDbUrl = process.env.MONGODB_URL;
+const MongoClient = require('mongodb').MongoClient;
+let db;
+
+app.set('view engine', 'ejs');
+
+// 0. DB Connection
+MongoClient.connect(mongDbUrl, { useUnifiedTopology: true }, (에러, client) => {
+  if (에러) {
+    return console.log(에러);
+  }
+
+  db = client.db('todoapp');
+  app.listen(8080, () => console.log('listening on 8080'));
 });
 
-// get
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-app.get('/write.html', function (req, res) {
-  res.sendFile(__dirname + '/write.html');
+// 1. Get Index.html
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+// 2. Get Write page
+app.get('/write', (req, res) => res.sendFile(__dirname + '/write.html'));
+
+// 3. Get List page
+app.get('/list', (req, res) => {
+  db.collection('post')
+    .find()
+    .toArray((error, result) => {
+      res.render('list.ejs', { posts: result });
+    });
 });
 
-app.get('/pet', function (req, res) {
-  res.send('펫 용품 쇼핑페이지입니다.');
-});
-
-app.get('/beauty', function (req, res) {
-  res.send('뷰티 용품 쇼핑페이지입니다.');
+// 4. Post To Do
+app.post('/add', (req, res) => {
+  db.collection('post').insertOne(
+    { 할일: req.body.title, 날짜: req.body.date },
+    () => console.log('저장 완료')
+  );
 });
